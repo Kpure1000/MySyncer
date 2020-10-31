b1b54ae19fba44e693e92c186d5a70f19fc8e436
Kpurek <kpurek@KpurekdeMacBook-Air.local>
2020-10-31 16:09:56 +0800

    commit ClientNetwork

---
+++ Assets/Scripts/ClientNetwork.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Text;
+
+public class ClientNetwork : MonoBehaviour
+{
+
+    public void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// Start Send Package
+    /// </summary>
+    /// <param name="selfPort"></param>
+    /// <param name="targetPort"></param>
+    public void StartSend(int selfPort, int targetPort)
+    {
+        if (canSend) return;
+
+        udpClient = new UdpClient(selfPort);
+
+        targetPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), targetPort);
+
+        canSend = true;
+    }
+
+    private IPEndPoint endPoint;
+
+    private byte[] recvBuf;
+    private string recvStr;
+
+    private bool readed;
+
+    public void StartReceive(int targetPort)
+    {
+        if (canReceive) return;
+
+        udpReceiv = new UdpClient();
+
+        new Thread(() =>
+        {
+            while (canReceive)
+            {
+                try
+                {
+                    udpReceiv.BeginReceive(new System.AsyncCallback((System.IAsyncResult ar) =>
+                    {
+                        recvBuf = udpReceiv.EndReceive(ar, ref endPoint);
+                        recvStr += Encoding.Default.GetString(recvBuf);
+                        readed = true;
+                    }), null);
+                }
+                catch (System.Exception ex)
+                {
+                    Debug.LogException(ex);
+                }
+            }
+        }).Start();
+    }
+
+    private void WriteData(string strData)
+    {
+        if (!canSend) return;
+        byte[] writeData = Encoding.Default.GetBytes(strData);
+        udpClient.Send(writeData, writeData.Length,targetPoint);
+    }
+
+    private string ReadData()
+    {
+        if(readed)
+        {
+            readed = false;
+            recvStr = "";
+            return recvStr;
+        }
+        return "";
+    }
+
+    /*  *   */
+
+    UdpClient udpClient;
+
+    UdpClient udpReceiv;
+
+    /// <summary>
+    /// Write data target point
+    /// </summary>
+    IPEndPoint targetPoint;
+
+    /*  *   */
+
+    private int selfPort { get; set; }
+
+    private bool canSend { get; set; }
+
+    private bool canReceive { get; set; }
+
+}
